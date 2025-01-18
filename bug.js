b082cbad6a9d7cb52723abf9257b23b4ee58dabe
BugHunter-X <askzenai@gmail.com>
2025-01-18 01:40:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+function foo(x){
+  if(x === null) return 0;
+  return x.length;
+}
+console.log(foo(null)); // 0
+console.log(foo([1,2,3])); // 3
+console.log(foo(undefined)); // TypeError: Cannot read properties of undefined (reading 'length')
